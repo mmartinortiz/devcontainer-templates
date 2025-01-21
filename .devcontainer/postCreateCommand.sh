c701af9986ef4d2e899f31126e6326a18f5392b3
manolo <mmartinortiz@gmail.com>
2025-01-21 15:55:07 +0000

    docs: Include comments

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,8 +2,10 @@
 
 npm install -g @devcontainers/cli
 
+# Install pre-commit hooks
 pre-commit install --install-hooks
 
+# Eye candy for the command line
 echo "
 format = \"\$git_branch \$git_status\\n> \"
 " > ~/.config/starship.toml
