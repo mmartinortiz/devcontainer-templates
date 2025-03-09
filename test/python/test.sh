c53df432fcc363868ea2743025233c54c1b5e48a
manolo <mmartinortiz@gmail.com>
2025-03-09 15:34:29 +0000

    fix: enable uv autocompletion via feature

--- test/python/test.sh
+++ test/python/test.sh
@@ -5,6 +5,8 @@ source test-utils.sh
 # Template specific tests
 check "distro" lsb_release -c
 check "version" python -V | grep 3.13
+check "uv autocomplete fish" [ -e /usr/share/fish/completions/uv.fish ]
+
 
 # Report result
 reportResults
